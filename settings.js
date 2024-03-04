fc3d714b98efba084f5207e3b147cf24770ca9a0
panel1810 <kuruminightmare50@gmail.com>
2024-03-04 19:18:32 +0700

    Bot

---
+++ settings.js
@@ -0,0 +1,73 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const version = require("@adiwajshing/baileys/package.json").version 
+global.require = require
+global.language = "id"
+global.nomerOwner = "33757050057"// Ganti sesuka luh
+global.nomerOwner2 = "6285763590930"// Ganti sesuka luh
+global.runWith = "Ubuntu" // Ganti sesuka luh
+global.ownerName = "Kurumi." // Ganti sesuka luh
+global.fakeUrl = ''// Ganti sesuka luh
+global.TextT = `Gαмσηᴮᵒᵗ` // Ganti sesuka luh
+global.botName = "Gαмσηᴮᵒᵗ"// Ganti sesuka luh
+global.sessionName = "session"
+global.setmenu = "livelocation"
+//global.docType = ""
+global.Qoted = "m"
+global.baileysMd = true
+global.antiSpam = true
+global.fake = botName
+global.autoBio = true
+global.Console = false
+global.publik = true
+global.limitCount = 30 // Ganti sesuka luh
+global.gcounti = { 'prem' : 60, 'user' : 20 } // Ganti sesuka luh
+global.copyright = `© Gαмσηᴮᵒᵗ` // Ganti sesuka luh
+global.baileysVersion = `Baileys version ${version}ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`
+global.textT = `Baileys version ${version}ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`
+global.fake1 = "Gαмσηᴮᵒᵗ" // Ganti sesuka luh
+global.packName = "Gαмσηᴮᵒᵗ" // Ganti sesuka luh
+global.authorName = "Created By єнαηz" // Ganti sesuka luh
+global.replyType = "web" // Setting => mess,web,web2,quoted 
+global.setwelcome = "type11"
+global.autoblockcmd = false
+global.autoReport = true
+global.autoLevel = true
+global.multi = true
+global.autoSticker = false
+global.Intervalmsg = 1000 // Detik
+global.app = '•' // Ganti sesuka luh
+global.apz = '⎘' // Ganti sesuka luh
+global.teksMenu = ''
+global.apiflash = '9b9e84dfc18746d4a19d3afe109e9ea4'
+global.Dana = '6285811965686'
+global.syt = 'https://www.youtube.com/'
+global.sgc = 'https://chat.whatsapp.com/Fguw4KxsP6qCBm9RfZvHO5'
+global.sig = 'https://instagram.com/'
+global.filestackApi ="" //daftar di filestack
+
+global.ehanzUrl = 'https://telegra.ph/file/59a2583b604f3cb255cb4.jpg'
+global.linkUrl = 'https://chat.whatsapp.com/C3zzPfb18BbA7LfRv6pjDz' // Ganti sesuka luh
+// Bisa ganti juga => https://wa.me/6281316643491
+//===========================================================//
+//APIKEY NYA KALIAN DAFTAR DULU KALO BLOM PUNYA GA ADA APIKEY FITUR GA BERJALAN LANCAR
+global.api = {
+ehz: '', //skizo.tech | kalo mau pake api gua izin dulu
+angel: 'zenzkey_8bb60993ae', // Apikey Zahwazein
+Lol: 'GataDios',
+Botcahx: 'Admin',
+ApikeyAi: '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',
+ApiNobg: 'r1DRrnqgdPT4ceHLUo9m4hTa',
+FilestackApi: '',
+}
+//👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+//https://www.skizo.tech [ Api ehz ]
+//https://www.remove.bg/id/api [ ApiNobg ]
+//https://www.filestack.com/ [ FilestackApi ]
+//https://platform.openai.com/account/api-keys [ ApikeyAi ]
+//===========================================================//
+
+
+
+
+
